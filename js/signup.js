bcf67a0a53c21625b08c560a3e6a2f5e95d8b019
hellonayeon <hellonykwon@gmail.com>
2022-06-24 18:22:29 +0900

    add: 자바 스크립트 함수 선언, 구글 폰트 추가

---
+++ js/signup.js
@@ -0,0 +1,16 @@
+
+/* 회원가입 처리 */
+function signup() {
+    console.log("[signup] func signup()");
+
+    console.log("회원가입 처리");
+    // if (isValidId() || isValidPassword() || isValidName() || isValidEmail()) {
+    //     location.href = "./login.html";
+    // }
+    // else {
+    //
+    // }
+
+
+}
+
---
+++ js/valid-check.js
@@ -0,0 +1,22 @@
+<!-- 아이디 유효성 검사 -->
+function isValidId() {
+    console.log("[valid-check] func isValidId()");
+
+}
+
+<!-- 비밀번호 유효성 검사 -->
+function isValidPassword() {
+    console.log("[valid-check] func isValidPassword()");
+
+}
+
+<!-- 이름 유효성 검사 -->
+function isValidName() {
+    console.log("[valid-check] func isValidName()");
+
+}
+
+<!-- 이메일 유효성 검사 -->
+function isValidEmail() {
+    console.log("[valid-check] func isValidEmail()");
+}
