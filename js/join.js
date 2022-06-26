0b2e53343a9604878e508a941cee2874ce3ff28b
hellonayeon <hellonykwon@gmail.com>
2022-06-27 00:49:56 +0900

    add: favicon 추가, 배경 이미지 추가

--- js/join.js
+++ js/join.js
@@ -3,14 +3,5 @@
 function join() {
     console.log("[signup] func signup()");
 
-    console.log("회원가입 처리");
-    // if (isValidId() || isValidPassword() || isValidName() || isValidEmail()) {
-    //     location.href = "./login.html";
-    // }
-    // else {
-    //
-    // }
-
-
 }
 
